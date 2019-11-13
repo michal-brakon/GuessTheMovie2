59b13cb885d0d7d42a737fe4745a0be7d8736722
Michal <zielonydeveloper@o2.pl>
2019-11-13 18:19:07 +0100

    first try not sur if its ok

--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -1,2 +1,15 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class GuessTheMovie {
-}
+    public static void main(String[] args) throws Exception {
+        File file = new File("movies.txt");
+        Scanner getMovieName = new Scanner (file);
+        while (getMovieName.hasNextLine()) {
+            String nameOfMovie = getMovieName.next(); } // bierze jedną z lini
+
+
+
+        }
+
+    }
