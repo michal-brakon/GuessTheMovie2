be3d0824c4afcb1a6f9c9c0e0f35bfd702cfcc1e
Michal <zielonydeveloper@o2.pl>
2019-11-14 09:32:49 +0100

    gets 25 random numbers of lines

--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -1,12 +1,19 @@
 import java.io.File;
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheMovie {
     public static void main(String[] args) throws Exception {
         File file = new File("movies.txt");
+        int c = 0;
         Scanner getMovieName = new Scanner (file);
         while (getMovieName.hasNextLine()) {
-            String nameOfMovie = getMovieName.next(); } // bierze jedną z lini
+            String nameOfMovie = getMovieName.nextLine();
+            c++;
+            Random random = new Random();
+            int r = random.nextInt(c);
+            System.out.println(r);
+            }
 
 
 
