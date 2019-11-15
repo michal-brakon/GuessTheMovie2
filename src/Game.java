62590572f9bc76f18499a74643e608bf3bc8470f
Michal <zielonydeveloper@o2.pl>
2019-11-15 16:53:30 +0100

    stuck

--- src/Game.java
+++ src/Game.java
@@ -6,21 +6,24 @@ import java.util.*;
 
 
 public class Game {
-    public Game(File file) {
-    }
-
-    public static String randomLIne(File f) throws IOException {
+    public static String randomLine() throws IOException {
         String result = null;
         Random random = new Random();
+        File f = new File("movies.txt");
         int n = 0;
-        for (Scanner sc = new Scanner(f); sc.hasNext(); ) {
+        for (Scanner sc = new Scanner(f);
+             sc.hasNext(); ) {
             n++;
             String line = sc.nextLine();
             if (random.nextInt(n) < n) {
                 result = line; }
-
         }
         return result;
-
     }
+    public static String underscoreAll () throws IOException {
+        String st = randomLine().replaceAll("[a-zA-Z]", "_");
+        return st;
+    }
+
+
 }
--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -1,19 +1,11 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
+
 
 public class GuessTheMovie {
     // tylko głowna metoda
 
-    public static void main(String[] args) throws Exception {
-
-        Game rf = new Game(new File("movie.txt"));
-        System.out.println(rf);
-         }
+    public static void main(String[] args) {
+        Game fg = new Game();
+        System.out.println(fg);
 
     }
-
-
-
-
-
+}
